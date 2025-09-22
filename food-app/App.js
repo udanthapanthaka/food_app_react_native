3d1767dce16a2f24202dc806fe75e0d1be69e8f4
udantha <udanthapanthaka12@gmail.com>
2025-09-22 14:47:17 +0530

    update App.js

--- food-app/App.js
+++ food-app/App.js
@@ -1,3 +1,7 @@
+import AppNavigator from './src/navigation/AppNavigator';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';  
+
 export default function App() {
   return <AppNavigator />;
 }
